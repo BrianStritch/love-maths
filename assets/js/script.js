1c1dbd2f78c88cf7fbe8cb5a6f3defbee96fe5c0
BrianStritch <brians12ca18@gmail.com>
2021-10-04 20:26:24 +0000

    add code for subtraction game, add majority of code to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,9 @@ function runGame(gameType){
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply'){
         displayMultiplyQuestion(num1, num2);
-    } else if (gameType === 'division'){
+    } else if (gameType === 'subtract'){
+        displaySubtractQuestion(num1, num2);
+    }else if (gameType === 'division'){
         displayDivisionQuestion(num1, num2);
     }else {
         alert(`Unknown game type: ${gameType}`);
@@ -66,6 +68,8 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition" ];
     } else if (operator === 'x'){
         return [operand1 * operand2, "multiply" ];
+    } else if (operator === '-'){
+        return [operand1 - operand2, "subtract" ];
     } else if (operator === '/'){
         return [operand1 / operand2, "division" ];
     } else {
@@ -96,8 +100,8 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operator').textContent = '+';
 }
 function displaySubtractQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = '-';
 }
 function displayMultiplyQuestion(operand1, operand2){
@@ -106,7 +110,7 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operator').textContent = 'x';
 }
 function displayDivisionQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = '/';
 }
